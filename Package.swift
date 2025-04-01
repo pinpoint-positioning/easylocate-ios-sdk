828789bf602c976371ec93c0eb544982f28deeb4
schech1 <76683319+schech1@users.noreply.github.com>
2025-04-01 12:09:03 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "pinpoint-easylocate-sdk",
-            targets: ["SDK"]),
+            targets: ["pinpoint-easylocate-sdk"]),
     ],
     dependencies: [],
     targets: [

08cde609de15fac1dc1ae63d8f3d70903ba63c0f
schech1 <76683319+schech1@users.noreply.github.com>
2025-06-25 15:27:29 +0200

    Checksum fix

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "pinpoint-easylocate-sdk",
-            url: "https://github.com/pinpoint-positioning/easylocate-ios-sdk/releases/download/12.2.1/pinpoint_easylocate_sdk.xcframework.zip",
+            url: "https://github.com/pinpoint-positioning/easylocate-ios-sdk/releases/download/12.2.2/pinpoint_easylocate_sdk.xcframework.zip",
 
-            checksum: "e8c2764ae87540b13b51bc4ae2ea87ec042e28d920fcac64c63594e084fdd708"
+            checksum: "226701204c5ca74c7a2af92fb5da4f480b30c071349502b4b37e1acc8f39315a"
         )
     ]
 )
